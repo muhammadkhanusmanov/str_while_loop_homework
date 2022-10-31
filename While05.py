89ac2e0a2ebc0864ea320c16adff2223e477aa4b
muhammadkhanusmanov <uqaxromon@gmail.com>
2022-10-31 09:43:15 +0500

    5-misol

--- While05.py
+++ While05.py
@@ -6,4 +6,10 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    i=0
+    n=0
+    while n<len(s):
+        if s[n].lower()==s[n]:
+            i+=1
+        n+=1
+    return i
